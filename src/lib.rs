9a599f08f100ab1bc8d5ad5701d4b57e969acefb
Andrey Vesnovaty <andrey.vesnovaty@replix.io>
2020-01-29 15:05:48 +0000

    Bump ver to 0.6
    Signed-off-by: Andrey Vesnovaty <andrey.vesnovaty@replix.io>

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 // Use is subject to license terms.
 //
 
-#![doc(html_root_url = "https://docs.rs/pmdk/0.5.1")]
+#![doc(html_root_url = "https://docs.rs/pmdk/0.6.0")]
 #![warn(clippy::use_self)]
 #![warn(deprecated_in_future)]
 #![warn(future_incompatible)]
